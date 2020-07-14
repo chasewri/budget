5a53802354bc8e25f01a5702adbde35cd5bdebaf
chasewri <chasewri@gmail.com>
2020-07-14 13:18:08 -0600

    add cors middleware

--- server.js
+++ server.js
@@ -15,6 +15,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(Auth);
 
 app.use(
