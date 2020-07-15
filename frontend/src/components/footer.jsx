87e7c51463e3aff8502cb04b58723e072bb9bf78
chasewri <chasewri@gmail.com>
2020-07-15 14:25:21 -0600

    ended up working on the index page

---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            FDV | You Deserve a Better Budget
+        </footer>
+    )
+}
+
+export default Footer
