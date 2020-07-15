d88a206c5b86aab49f16a36427b703b0627adf3d
chasewri <chasewri@gmail.com>
2020-07-14 19:07:12 -0600

    index page start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import IndexPage from './pages/indexPage'
+
+
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+    <Switch>
+      <Route path='/' component={IndexPage} exact />
+    </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App;
